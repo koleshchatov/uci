73780ee6b55426db030273540b9d3e5d2f85e397
dit <dit@dit.com>
2025-03-17 20:25:08 +0300

    added: pashalka

--- my-site/src/Pages/HomePage.jsx
+++ my-site/src/Pages/HomePage.jsx
@@ -1,24 +1,22 @@
+import Picture from "/Components/Pictures/Picture.jsx";
+import { ImageContainer } from "/Components/Pictures/ImageContainer.jsx";
+import React from "react";
+import PidorModal from "/Components/PidorModal.jsx";
+import Title from "/Components/Title.jsx";
 
-import Picture from '/Components/Pictures/Picture.jsx'
-import { ImageContainer } from '/Components/Pictures/ImageContainer.jsx'
-import React from 'react'
-import PidorModal from '/Components/PidorModal.jsx'
-import Title from '/Components/Title.jsx'
-
-export default function HomePage (){
-   
+export default function HomePage() {
+  console.log(" pashalka HUI");
   return (
     <>
-
       <Title />
 
-      <div style={{display: 'flex'}}>
-          {Object.entries(ImageContainer).map(([key, value]) => <Picture image = {value} key={key}/>)}
-      </div>      
+      <div style={{ display: "flex" }}>
+        {Object.entries(ImageContainer).map(([key, value]) => (
+          <Picture image={value} key={key} />
+        ))}
+      </div>
 
-      <PidorModal />  
-        
-      
+      <PidorModal />
     </>
-  )
+  );
 }
